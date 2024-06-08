d145f2d70c903690ae2bec2e9372d533f754b61b
Eyo Chen <eyo.chen@amazingtalker.com>
2024-06-08 17:57:05 +0800

    feat: first commit

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Response structure
+type Response struct {
+	Message string `json:"message"`
+}
+
+// helloHandler responds with a hello message
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello, World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+// goodbyeHandler responds with a goodbye message
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Goodbye, World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/api/hello", helloHandler)
+	http.HandleFunc("/api/goodbye", goodbyeHandler)
+
+	log.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OYE0303/go-deploy
+
+go 1.21.4
