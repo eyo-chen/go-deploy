a72115645f6528f8046ed7ebd5298749b96c5aec
Eyo Chen <eyo.chen@amazingtalker.com>
2024-07-14 17:51:48 +0800

    feat: setup db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
 )
 
 // Response structure
@@ -34,6 +38,22 @@ func main() {
 	http.HandleFunc("/api/hello", helloHandler)
 	http.HandleFunc("/api/goodbye", goodbyeHandler)
 
+	log.Println("connecting to database...")
+	db, err := newMysqlDB()
+	if err != nil {
+		log.Fatalf("Could not connect to database: %s\n", err.Error())
+	}
+
+	log.Println("applying schema migrations...")
+	if err := applySchemaMigrations(db); err != nil {
+		log.Fatalf("Could not apply schema migrations: %s\n", err.Error())
+	}
+
+	log.Println("applying data migrations...")
+	if err := applyDataMigrations(db); err != nil {
+		log.Fatalf("Could not apply data migrations: %s\n", err.Error())
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4040"
@@ -42,4 +62,70 @@ func main() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
+}
+
+func newMysqlDB() (*sql.DB, error) {
+	config := map[string]string{
+		"name":     os.Getenv("DB_NAME"),
+		"user":     os.Getenv("DB_USER"),
+		"password": os.Getenv("DB_PASSWORD"),
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:54321)/%s?parseTime=true", config["user"], config["password"], config["name"])
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func applySchemaMigrations(db *sql.DB) error {
+	driver, err := mysql.WithInstance(db, &mysql.Config{
+		MigrationsTable: "schema_migrations",
+	})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations/schema/",
+		"mysql", driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
+
+func applyDataMigrations(db *sql.DB) error {
+	driver, err := mysql.WithInstance(db, &mysql.Config{
+		MigrationsTable: "data_migrations",
+	})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations/data/",
+		"mysql", driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/OYE0303/go-deploy
 
 go 1.21.4
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+)
